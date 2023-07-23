ec5265bd24a204a880b06d1c3a692afd93f3465a
Sudiptto Biswas <sudiptto.biswas@bam-academy.com>
2023-07-23 18:45:04 -0400

    length issues

--- cracked.py
+++ cracked.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template, request
 import PyPDF2
 from PyPDF2 import PdfReader
+import openai
+from passwords import *
 import re
+
+
 
 # Create the Flask application
 app = Flask(__name__)
+openai.api_key = openai_key
+
+
+# openai
+
+def text_chunk(txt_prompt):
+    # Generate text using the OpenAI API
+    response = openai.Completion.create(
+        engine='text-davinci-003',  # Specify the model to use
+        prompt=txt_prompt,
+        n=1,  # Limit to a single completion
+        max_tokens=2000,  # Limit the generated text to around two thousand tokens
+        truncation='only',  # Truncate the input text to fit within the model's token limit
+    )
+
+    # Get the generated text from the API response
+    generated_text = response.choices[0].text.strip()
+
+    # Print the generated text
+    return generated_text
+
 
 # Define a route and its handler
 @app.route('/')
@@ -32,9 +57,28 @@ def upload():
             text = re.sub(r'\/', '', text)
 
             all_text += text.strip()
-        print(len(reader.pages))
-        print(all_text)
 
+        
+
+        #print(len(all_text))
+        total_length = len(all_text)
+        print(total_length)
+        print(type(all_text))
+
+        if total_length > 10000:
+            print("This is to much instead we are using the first 10,000 characters")
+            new_text = all_text[:10000] # first 10,000 characters
+ 
+            print(len(new_text))
+        #print(len(reader.pages))
+        #print(all_text)
+
+       
+        """txt_prompt = f'Based on this text, only generate 5 relevant questions based on the text and only print out the 5 questions based on the text: {all_text}'
+        val = text_chunk(txt_prompt)
+        print(val)"""
+
+        
         return f"File '{filename}' uploaded successfully!"
         
     else:
@@ -50,3 +94,4 @@ if __name__ == '__main__':
 # THIS WORKS ONLY FOR PDFS WHERE YOU CAN COPY AND PASTE THE TEXT NOT ON PDFS WHERE YOU HAVE TO TAKE PICTURES OF THE TEXT
 
     
+print(len('sdsds'))
